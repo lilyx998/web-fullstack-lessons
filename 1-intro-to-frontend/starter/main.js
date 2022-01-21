b4c9b2aa7f9865209171ac9c85f2e21d99d9907a
lilyang <lilyang@umich.edu>
2022-01-20 20:48:55 -0500

    added input, button, and note adding feature

--- 1-intro-to-frontend/starter/main.js
+++ 1-intro-to-frontend/starter/main.js
@@ -1,6 +1,21 @@
 const noteForm = document.getElementById("note-form");
+const titleInput = document.getElementById("title-input");
+const bodyInput = document.getElementById("body-input");
 
 // make me add more notes on submit
 noteForm.onsubmit = function(event) {
     event.preventDefault();
+    
+    var titleNode = document.createElement("h3");
+    titleNode.innerText = titleInput.value; 
+
+    var bodyNode = document.createElement("p");
+    bodyNode.innerText = bodyInput.value; 
+
+    var articleNode = document.createElement("article"); 
+    articleNode.appendChild(titleNode); 
+    articleNode.appendChild(bodyNode); 
+
+    var notesDiv = document.getElementById("notes");
+    notesDiv.appendChild(articleNode); 
 }
